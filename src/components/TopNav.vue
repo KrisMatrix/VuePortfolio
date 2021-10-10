e9b361df564eb483c0fd9431088013597c1db7d6
MeganZC <58228133+MeganZC@users.noreply.github.com>
2021-10-10 18:05:39 -0400

    Update TopNav.vue

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="topnav">
-    <div style="background-color: blue; cursor: pointer;"
+    <div style="background-color: #419c59; cursor: pointer;"
       @click="toggleSideNav()">Menu</div>
     <div>EngrExpo (Work In Progress)</div>
     <!--
-    <div style="background-color: blue; cursor: pointer;">Settings</div>
+    <div style="background-color: #419c59; cursor: pointer;">Settings</div>
     -->
   </div>
 </template>
 
 <style>
 .topnav {
-  background-color: black;
+  background-color: #333333;
   color: white;
   width: 100%;
   text-align: center;
-  font-size: 2em;
+  font-size: 1.5em;
   display: grid;
   grid-template-columns: 1fr 10fr 1fr
 }
