6aeac5353f4e094faab2942842054cbcb2c43217
MeganZC <58228133+MeganZC@users.noreply.github.com>
2021-10-10 18:06:00 -0400

    Update AboutMe.vue

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="block">
-    <h2>About Me</h2>
+    <h2>Hi! I'm Krishna</h2>
     <div class="aboutMeBlock">
     <img src="@/assets/images/profile.jpeg" alt="Picture of Krishna">
-    <p>Hi! I'm Krishna!</p>
+    <p>I built this portfolio to showcase some of the work I've done in various area of interest. It serves as a technical repo of some of the projects I've done, but I also wanted a place to house some of my creative projects as well.
+    Filler content Just trying to see how this looks in multiple paragraphs
+    Filler content Just trying to see how this looks in multiple paragraphs
+    Filler content Just trying to see how this looks in multiple paragraphs
+    Filler content Just trying to see how this looks in multiple paragraphs
+    Filler content Just trying to see how this looks in multiple paragraphs</p>
     </div>
   </div>
 </template>
@@ -11,16 +16,19 @@
 <style scoped>
 .block {
   text-align: center;
+  background: #6db07f;
 }
 
 .aboutMeBlock {
   text-align: center;
   display: grid;
   grid-template-columns: 1fr 1fr;
+  padding: 5%;
 }
 
 .aboutMeBlock img {
   border-radius: 100%;
+  padding: 3%;
 }
 </style>
 
