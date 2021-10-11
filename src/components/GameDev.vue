d4e5da2e9179510272d4281fcc942dfcbcf04854
MeganZC <58228133+MeganZC@users.noreply.github.com>
2021-10-11 10:28:36 -0400

    Update GameDev.vue

--- src/components/GameDev.vue
+++ src/components/GameDev.vue
@@ -1,38 +1,27 @@
 <template>
   <div class="gameBlock">
   <h2>Game Development</h2>
-  <p>I have always been fascinated with video games since I was child.  I
-  enjoy playing video games even today, although my ability to play through
-  a marathon length video game has certainly diminished.</p>
+  <p>I have been fascinated with video games since I was child.  I
+  enjoy playing video games even today...though my ability to play through
+  a marathon length video game has certainly diminished!</p>
 
-  <p>I have always wanted to learn video game development, but without proper
-  mentors who could guide me to study the right subjects for game development,
-  and without adults who were willing to take my interest with some
-  seriousness, I never got the chance to learn and build a video game until
-  2021, when I participated in the San Diego Global Game Jam!!</p>
+  <p>I have always wanted to learn video game development, but without adults who were willing to take my interest with some
+  seriousness, I stuck to playing them when I was a kid. I had my first opportunity to build a video game in 2021 when I participated in the San Diego Global Game Jam.</p>
 
-  <p>The San Diego Global Game jam is a bi-annual event where you get to meet
-  up with other game creator enthusiasts to form a team and work one or more
-  components of game design. These include music, character creation, level
-  design, AI, and game development.</p>
+  <p>The San Diego Global Game Jam is a bi-annual event where you get to meet
+  up with other video game enthusiasts to form a team and collaborate on game design. Everyone focuses on different components, including music, character creation, level
+  design, AI, story creation, and more.</p>
 
-  <p>You can check out the game in the Global Game Jam channel
+  <p>You can check out the game my team created in the Global Game Jam channel
     <a href="https://v3.globalgamejam.org/2021/games/cosmoball-4">here</a>.
   </p>
 
   <img src="@/assets/images/gameDevMenu.png" alt="Cosmo Ball Menu">
 
-  <p> I was responsible for the following parts:</p>
-  <ul>
-    <li>Level Design</li>
-    <li>Lighting and Shadow</li>
-    <li>Ball movement</li>
-  </ul>
+  <p> We opted for a classic Super Mario Bros. type style just to get our feet wet. I was primarily responsible for level design, lighting and shadow, and ball movement while my fellow teammates worked on menus, music, character design, level components, and art.</p>
 
   <img src="@/assets/images/game_still1.png" alt="Cosmo Ball Gameplay 1">
 
-  <p>My fellow team mates did the menus, music, character design, level
-    components and art.</p>
 
   <img src="@/assets/images/game_still2.png" alt="Cosmo Ball Gameplay 2">
   </div>
@@ -42,6 +31,7 @@
 .gameBlock {
   text-align: center;
   font-size: 1.5em;
+  padding: 2%;
 }
 </style>
 
